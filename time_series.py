20f3f95ccfb95b3ab0288df89caadf92082f6ac5
unicorn unicorn <unicorn1579@proton.me>
2025-12-19 03:55:07 +0300

    MLE-2. first models

--- time_series.py
+++ time_series.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 from pandas.tseries.frequencies import to_offset
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 from statsmodels.tsa.seasonal import seasonal_decompose
+import xgboost as xgb
 
 
 class TimeSeriesTransformer(BaseEstimator, TransformerMixin):
@@ -144,6 +147,24 @@ class TimeSeriesTransformer(BaseEstimator, TransformerMixin):
 
 
 @dataclass
+class ModelMetadata:
+    """
+    Инициализирует объект ModelMetadata.
+
+    Args:
+        model_type (str): тип модели
+        model (LinearRegression): модель
+        y_test (): тестовый набор данных
+        y_prediction (): спрогнозированный набор данных
+        metrics (Dict[str, float]): метрики качества модели
+    """
+    model_type: str = None
+    model: LinearRegression = None
+    y_test: List[float] = None
+    y_prediction: List[float] = None
+    metrics: Dict[str, float] = None
+
+@dataclass
 class TimeSeries:
     """
     Инициализирует объект TimeSeries.
@@ -192,7 +213,12 @@ class TimeSeries:
     data: pd.DataFrame = None
     data_transformed: pd.DataFrame = None
     data_decomposed:  pd.DataFrame = None
+    data_processed_columns: Dict[str, pd.DataFrame] = None
 
+    correlation_method: str = "spearman"
+    correlation_threshold: float = 0.3
+
+    models_metadata: Dict[str, List[ModelMetadata]] = None
 
     def __post_init__(self):
         if self.file_name and self.file_extension:
@@ -207,6 +233,8 @@ class TimeSeries:
         self.read()
         self.transform()
         self.decompose()
+        self.features_generation()
+        self.select_significant_features()
 
     def read(self) -> None:
         """
@@ -238,7 +266,9 @@ class TimeSeries:
         Разложение временных рядов на тренд, сезонность и остаток
         """
         data_decomposed = {}
+        self.data_processed_columns = {}
         for column in self.processed_columns:
+            self.data_processed_columns[column] = self.data_transformed[[column]].copy()
             column_decomposed = seasonal_decompose(
                 self.data_transformed[column],model=self.decompose_model,period=self.decompose_period
             )
@@ -246,8 +276,13 @@ class TimeSeries:
             data_decomposed[f"{column}__trend"] = column_decomposed.trend
             data_decomposed[f"{column}__seasonal"] = column_decomposed.seasonal
             data_decomposed[f"{column}__resid"] = column_decomposed.resid
-        self.data_decomposed = pd.DataFrame(data_decomposed)
 
+            self.data_processed_columns[column]["observed"] = column_decomposed.observed
+            self.data_processed_columns[column]["trend"] = column_decomposed.trend
+            self.data_processed_columns[column]["seasonal"] = column_decomposed.seasonal
+            self.data_processed_columns[column]["resid"] = column_decomposed.resid
+            self.data_processed_columns[column] = self.data_processed_columns[column].drop(column, axis=1)
+        self.data_decomposed = pd.DataFrame(data_decomposed)
 
     def show(self) -> None:
         """
@@ -300,4 +335,137 @@ class TimeSeries:
         axes[-1].set_xlabel("Дата", fontsize=11)
         fig.suptitle(f"Разложение данных временного ряда {self.file_name}", fontsize=15, y=0.99)
         plt.tight_layout(rect=[0, 0, 1, 0.97])
+        plt.show()
+
+    def features_generation(self) -> None:
+        """
+        Генерация признаков применительно к каждой обрабатываемой колонки исходных данных
+        """
+        for column in self.processed_columns:
+            # Календарные признаки
+            self.data_processed_columns[column]["day"] = self.data_processed_columns[column].index.day
+            self.data_processed_columns[column]["dayofweek"] = self.data_processed_columns[column].index.dayofweek  # 0-6
+            self.data_processed_columns[column]["weekofyear"] = self.data_processed_columns[column].index.isocalendar().week.astype(int)
+            self.data_processed_columns[column]["month"] = self.data_processed_columns[column].index.month
+            self.data_processed_columns[column]["quarter"] = self.data_processed_columns[column].index.quarter
+            # Циклические признаки
+            self.data_processed_columns[column]["dow_sin"] = np.sin(2 * np.pi * self.data_processed_columns[column].index.dayofweek / 7)
+            self.data_processed_columns[column]["dow_cos"] = np.cos(2 * np.pi * self.data_processed_columns[column].index.dayofweek / 7)
+            self.data_processed_columns[column]["month_sin"] = np.sin(2 * np.pi * self.data_processed_columns[column].index.month / 12)
+            self.data_processed_columns[column]["month_cos"] = np.cos(2 * np.pi * self.data_processed_columns[column].index.month / 12)
+            self.data_processed_columns[column]["dayofyear_sin"] = np.sin(2 * np.pi * self.data_processed_columns[column].index.dayofyear / 365)
+            self.data_processed_columns[column]["dayofyear_cos"] = np.cos(2 * np.pi * self.data_processed_columns[column].index.dayofyear / 365)
+            self.data_processed_columns[column]["hour_sin"] = np.sin(2 * np.pi * self.data_processed_columns[column].index.hour / 24)
+            self.data_processed_columns[column]["hour_cos"] = np.cos(2 * np.pi * self.data_processed_columns[column].index.hour / 24)
+            # Признаки сезонности
+            # сила сезонного эффекта
+            self.data_processed_columns[column]["seasonal__abs"] = self.data_processed_columns[column]["seasonal"].abs()
+            # фаза сезонности
+            self.data_processed_columns[column]["seasonal__phase"] = self.data_processed_columns[column]["seasonal"].rank(pct=True)
+            # дельта сезонности
+            self.data_processed_columns[column]["seasonal__diff"] = self.data_processed_columns[column]["seasonal"].diff()
+            # нормализованная сезонность
+            self.data_processed_columns[column]["seasonal__scaled"] = (self.data_processed_columns[column]["seasonal"] - self.data_processed_columns[column]["seasonal"].mean()) / self.data_processed_columns[column]["seasonal"].std()
+
+    def select_significant_features(self) -> None:
+        for column in self.processed_columns:
+            # Нормализация только тренда
+            trend = self.data_processed_columns[column]["trend"]
+            self.data_processed_columns[column]["trend"] = (trend - trend.mean()) / trend.std(ddof=0)
+
+            # Корреляция признаков с данными наблюдений (observed)
+            correlation = self.data_processed_columns[column].corr(method=self.correlation_method)[["observed"]].drop("observed")
+            # Отбор значимых признаков
+            significant = correlation[correlation["observed"].abs() > self.correlation_threshold]
+            selected_features = significant.index.tolist() + ["observed"]
+            self.data_processed_columns[column] = self.data_processed_columns[column][selected_features]
+
+    def rolling_models(self, train_ratio: float = 0.9, n_runs: int = 10):
+        self.models_metadata = dict()
+        for column in self.processed_columns:
+            self.models_metadata[column] = list()
+
+            data = self.data_processed_columns[column].dropna()
+            # размер тестового окна
+            train_size = int(len(data) * train_ratio)
+            step = (len(data) - train_size) // n_runs
+            if step <= 0:
+                continue
+
+            for i in range(n_runs):
+                # граница train-выборки
+                end_train = train_size + i * step
+                train = data.iloc[:end_train]#.dropna()
+                test = data.iloc[end_train:end_train + step]#.dropna()
+
+                if len(train) == 0 or len(test) == 0:
+                    continue
+
+                # разделение на признаки и таргет
+                x_train = train.drop(columns=["observed"])
+                y_train = train["observed"]
+                x_test = test.drop(columns=["observed"])
+                y_test = test["observed"]
+
+                # LinearRegression
+                model = LinearRegression()
+                model.fit(x_train, y_train)
+
+                y_prediction = model.predict(x_test)
+
+                self.models_metadata[column].append(
+                    ModelMetadata(
+                        model_type='LinearRegression',
+                        model=model,
+                        y_test=y_test,
+                        y_prediction=y_prediction,
+                        metrics={"MAE": mean_absolute_error(y_test, y_prediction)}
+                    )
+                )
+
+                # XGBRegressor
+                model = xgb.XGBRegressor(
+                    n_estimators=500,
+                    max_depth=5,
+                    learning_rate=0.1,
+                    subsample=0.8,
+                    colsample_bytree=0.8,
+                    random_state=42,
+                    eval_metric="mae",
+                    early_stopping_rounds=20
+                )
+
+                model.fit(
+                    x_train, y_train,
+                    eval_set=[(x_test, y_test)],
+                    verbose=False
+                )
+
+                y_prediction = model.predict(x_test)
+
+                self.models_metadata[column].append(
+                    ModelMetadata(
+                        model_type='XGBRegressor',
+                        model=model,
+                        y_test=y_test,
+                        y_prediction=y_prediction,
+                        metrics={"MAE": mean_absolute_error(y_test, y_prediction)}
+                    )
+                )
+
+    def show_metric(self, column_name: str, metric_name: str = 'MAE'):
+        n_models = len(self.models_metadata[column_name])
+        fig, axes = plt.subplots(n_models, 1, figsize=(15, 3 * n_models), sharex=True)
+        results = [(model_metadata.model_type, model_metadata.y_test, model_metadata.y_prediction, model_metadata.metrics[metric_name])
+                   for model_metadata in self.models_metadata[column_name]]
+
+        for ax, (model_type, y_test, y_prediction, metric) in zip(axes, results):
+            ax.plot(y_test.index, y_test, color="black", alpha=0.5, label="Observed")
+            ax.plot(y_test.index, y_prediction, color="red", label=f"Predicted ({metric_name}={metric:.2f})")
+            ax.set_title(model_type)
+            ax.grid(True)
+            ax.legend()
+
+        plt.xlabel(self.time_column)
+        plt.tight_layout()
         plt.show()
